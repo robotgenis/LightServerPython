9825a5377fdf2d9fa1700fb70afbec72d4fcd16e
Brandon Young <robotgenis@gmail.com>
2022-07-24 09:14:23 -0400

    save

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-app = Flask(__name__)
 
+import time, flask
+app = flask.Flask(__name__)
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+data = {}
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/set')
+def routeSet():
+    global data
+    data = {"timestamp": time.time(), "value": flask.request.args.get("value", default="")}
+    return data
+   
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/get')
+def routeGet():
+    global data
+    return data
 
 
 if __name__ == '__main__':
